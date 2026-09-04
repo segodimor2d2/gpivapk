0288530bd9c4ea046b7fc892fb1b4a70b50e8d88
Your Name <segodimo@gmail.com>
2026-09-04 17:49:28 -0300

    mpv_context_events modulo

---
+++ app/src/main/cpp/mpv_context_events.h
@@ -0,0 +1,14 @@
+#ifndef GPIV_MPV_CONTEXT_EVENTS_H
+#define GPIV_MPV_CONTEXT_EVENTS_H
+
+#include "mpv_context.h"
+
+bool mpv_context_events_start(
+    MpvContext* context
+);
+
+void mpv_context_events_stop(
+    MpvContext* context
+);
+
+#endif
